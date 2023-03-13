8dc0091dc22d87c3affea5e8b9cf676fc59dd6a3
ilgorbek <ilgorbek@gmail.com>
2023-03-13 16:18:21 -0400

    initial checkin

---
+++ traffic_light/main.py
@@ -0,0 +1,26 @@
+import machine
+import time
+
+green = machine.Pin(19, machine.Pin.OUT)
+yellow = machine.Pin(18, machine.Pin.OUT)
+red = machine.Pin(21, machine.Pin.OUT)
+relay = machine.Pin(23, machine.Pin.OUT)
+
+lights = ['green', 'yellow', 'red']
+
+for light in lights:
+    if light == 'green':
+        green.on()
+        relay.on()
+    elif light == 'yellow':
+        yellow.on()
+    elif light == 'red':
+        red.on()
+        relay.off()
+    print(light)
+    time.sleep(.5)
+    red.off()
+    yellow.off()
+    green.off()
+    time.sleep(.5)
+print("done!")
