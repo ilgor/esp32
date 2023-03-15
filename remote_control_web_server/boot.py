0ee36c40ab6712297d204804532b7640ff9b4e42
ilgorbek <ilgorbek@gmail.com>
2023-03-15 06:44:27 -0400

    +Added wifi connection

--- remote_control_web_server/boot.py
+++ remote_control_web_server/boot.py
@@ -1,1 +1,11 @@
-# initial wifi connection goes here
+import network
+import time
+
+wifi = network.WLAN(network.STA_IF)
+wifi.active(True)
+wifi.contact('wifi name', 'wifi password')
+
+while not wifi.isconnected():
+    time.sleep(1)
+
+print('Wifi connection was succesful! ', 'IP address: ', wifi.ifconfig()[0])
