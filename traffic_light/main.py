1a6b56bed4e44f4cd569add27639e7ccfe8a1af7
ilgorbek <ilgorbek@gmail.com>
2023-03-15 21:18:31 -0400

    runs twice now

--- traffic_light/main.py
+++ traffic_light/main.py
@@ -8,20 +8,21 @@ relay = machine.Pin(15, machine.Pin.OUT)
 
 lights = ['green', 'yellow', 'red']
 
-for light in lights:
-    if light == 'green':
-        green.on()
-        relay.on()
-    elif light == 'yellow':
-        yellow.on()
-    elif light == 'red':
-        red.on()
-        relay.off()
-    print(light)
-    time.sleep(.5)
-    red.off()
-    yellow.off()
-    green.off()
-    time.sleep(.5)
+for i in range(2):
+    for light in lights:
+        if light == 'green':
+            green.on()
+            relay.on()
+        elif light == 'yellow':
+            yellow.on()
+        elif light == 'red':
+            red.on()
+            relay.off()
+        print(light)
+        time.sleep(.5)
+        red.off()
+        yellow.off()
+        green.off()
+        time.sleep(.5)
 print("done!")
 
