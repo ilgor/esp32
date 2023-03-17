f9a421e5dc23e47d1fd23dc63cfa6ada2822116e
ilgorbek <ilgorbek@gmail.com>
2023-03-16 21:05:43 -0400

    Simple webserver to control ESP32 buildin LED remotely

--- remote_control_web_server/main.py
+++ remote_control_web_server/main.py
@@ -1,12 +1,14 @@
 import machine
-from microdot_asyncio import Microdot
+from microdot_asyncio import Microdot, Response, send_file
+from microdot_utemplate import render_template
 
 led_pin = machine.Pin(2, machine.Pin.OUT)
 app = Microdot()
+Response.default_content_type = 'text/html'
 
-@app.route('/')
-async def hello(request):
-    return 'Hello world'
+@app.get('/')
+async def index(request):
+    return render_template("index.html")
 
 @app.route('on')
 async def on(request):
@@ -14,9 +16,16 @@ async def on(request):
     return 'LED is ON'
 
 @app.route('off')
-async def on(request):
+async def off(request):
     led_pin.off()
     return 'LED is OFF'
+
+@app.route("/static/<path:path>")
+def static(request, path):
+    print(path)
+    if ".." in path:
+        return "Not found", 404
+    return send_file("static/" + path)
 
 def start_server():
     print('Starting microdot app')
---
+++ remote_control_web_server/static/js/index.js
@@ -0,0 +1,21 @@
+function callLedOn() {
+  fetch("/on")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+function callLedOff() {
+  fetch("/off")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
